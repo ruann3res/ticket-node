e701a1934d9ea8a4469b5826fc24f4f8d1038487
Ruan <Ruanpablo1218@gmail.com>
2022-02-24 11:11:11 -0300

    bcrypt para criptografia

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize'
+import bcrypt from 'bcrypt'
 
 
 class User extends Model {
@@ -6,7 +7,7 @@ class User extends Model {
     super.init({
       name: Sequelize.STRING,
       email: Sequelize.STRING,
-    
+      password: Sequelize.VIRTUAL,
       password_hash: Sequelize.STRING,
       admin: Sequelize.BOOLEAN
     },
@@ -14,9 +15,17 @@ class User extends Model {
       sequelize
     }
     )
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 10)
+      }
+    })
     return this
   }
 
+  checkPassword (password) {
+    return bcrypt.compare(password, this.password_hash)
+  }
 }
 
 export default User
